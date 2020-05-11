0b21c0aa7c222fdd21bd516a9bd0d8af2615e2e5
jaskokoyn <jack@jaskokoyn.com>
2020-05-11 17:48:48 -0400

    completed project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClassDirective } from './class.directive';
+import { LoopDirective } from './loop.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassDirective,
+    LoopDirective
   ],
   imports: [
     BrowserModule
